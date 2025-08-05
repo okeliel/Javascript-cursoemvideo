8fceb065e9d3544977cb018ec6e81950bdb06caf
Eliel Marques da Silva <elielmarques@protonmail.com>
2025-08-04 22:56:35 -0300

    04-08-2025

---
+++ aula 11/8-condicao_simples.js
@@ -0,0 +1,18 @@
+//CONDIÇÃO SIMPLES
+
+//Aqui não tem SENÃO (ELSE)
+
+
+    var vel = 19
+    console.log(`A velocidade do carro é ${vel}km/h.`)
+    if(vel > 60) {
+        console.log(`MULTADO! Você ultrapassou a velocidade permitida.`)
+}
+console.log(`Dirija sempre com cuidado!`)
+    
+
+//----------------------------------------------//
+
+
+
+
---
+++ aula 11/9-condicao_composta.js
@@ -0,0 +1,25 @@
+//CONDIÇÃO COMPOSTA
+
+// AQUI TEM O IF E O ELSE
+
+
+//var pais = `Grécia`
+//console.log(`você vive no(a) ${pais}.`)
+//if  (pais == `Brasil`) {
+//    console.log (`Você é brasileiro(a).`)
+//}
+//else {
+//    console.log(`Você é gringo(a).`)
+//}
+
+
+//------------------------------------------------
+
+var pais  = `Brasil`
+console.log(`Você vive no(a) ${pais}`)
+if (pais != `Brasil`) {
+    console.log (`Você é gringo.`)
+}
+else {
+    console.log (`Você é brasileiro(a).`)
+}
