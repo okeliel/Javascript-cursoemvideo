1236056019e605cc9860bc28fc03b9c4fe9493f3
Eliel Marques da Silva <elielmarques@protonmail.com>
2025-11-20 00:51:15 -0300

    20-11-2025

---
+++ Testes Alfa/aula 11/8-condicao_simples.js
@@ -0,0 +1,7 @@
+console.log ('Hello, world!')
+
+var vel = 60
+
+if (vel >= 60)     
+    console.log (`Sua velocidade: ${vel}`)
+    console.log ('Você está acima do limite. Multado!')
---
+++ Testes Alfa/aula 11/9-condicao_composta.js
@@ -0,0 +1,9 @@
+var p = 'gales'
+
+if (p !='Brasil') {
+console.log (`Você é gringo!`)
+}
+
+else {
+    console.log (`Você é brasileiro!`)
+}
