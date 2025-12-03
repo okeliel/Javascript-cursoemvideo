52d9aeeed43e8e65e494cb586fa78133546b50e4
Eliel Marques da Silva <elielmarques@protonmail.com>
2025-12-02 22:17:14 -0300

    02-12-2025

---
+++ aula 12/ex_do_tempo/cerebro.js
@@ -0,0 +1,25 @@
+function carregar () {
+
+   var msg = document.getElementById ('msg')
+   var img = document.getElementById('imagem')
+
+   var data = new Date ()
+   var hora = data.getHours()
+   msg.innerHTML = `Agora são ${hora} horas`
+
+   
+   if (hora >= 0 && hora < 12) {
+      //BOMDIA
+      document.body.style.background = '#4787CA'
+      img.src = 'imagens/dia.png'
+   } else if (hora >= 12 && hora < 18) {
+      // BOATARDE
+      img.src = 'imagens/tarde.png'
+      document.body.style.background = '#DD8E4C'
+   } else {
+      // BOANOITE
+      img.src = 'imagens/noite.png'
+      document.body.style.background = '#674E7B'
+   }
+
+}
