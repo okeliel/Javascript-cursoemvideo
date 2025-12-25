1c9eb278b81da47808dbcca41e64845f8cc83013
Eliel Marques da Silva <elielmarques@protonmail.com>
2025-12-24 21:34:05 -0300

    24-12-2025

--- aula 12/ex_de_idade/amarelo.js
+++ aula 12/ex_de_idade/amarelo.js
@@ -1,15 +1,50 @@
 function verificar () {
+    
     var data = new Date ()
     var ano = data.getFullYear()
     var fano = document.getElementById('ano')
     var res = document.querySelector ('div#res')
-    if (fano.Value.length == 0 || Number (fano.value) > ano) {
+    if (fano.value.length == 0 ||  fano.value > ano) {
     } else {
-        var fsexo = document.getElementById('sexo')
+        var fsexo = document.getElementsByName('radsex')
         var idade = ano - Number (fano.value)
-        var gênero = ''
         var img = document.createElement('img')
 
-        
+        img.setAttribute('id','foto')
+
+        if (fsexo[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) /*criança*/{
+            img.setAttribute('src','baby_boy.png')
+            } else if (idade < 21) /*jovem*/ {
+            img.setAttribute('src' , 'teen_boy.png')
+            } else if (idade < 50 ) /*adulto*/ {
+            img.setAttribute('src' , 'adult_boy.png')
+            } else {
+            img.setAttribute('src', 'old_boy.png') /*idoso*/ 
+            }
+        }   else if (fsexo [1]. checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) /*criança*/ {
+                img.setAttribute('src' , 'baby_girl.png')
+            } else if (idade < 21) /*jovem*/ {
+            img.setAttribute('src' , 'teen_girl.png')
+            } else if (idade < 50) {
+                img.setAttribute('src' , 'adult_girl.png')
+            } else {
+                img.setAttribute ('src' , 'old_girl.png')
+            }
+        } 
+
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.style.textAlign = 'center'
+        res.appendChild(img)
+
+        img.style.width = "180px";
+        img.style.height = "180px";
+
+        img.style.display = "block";
+        img.style.marginLeft = "auto";
+        img.style.marginRight = "auto";
     }
 }
