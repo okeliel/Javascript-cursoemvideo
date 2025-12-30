b6a4d2125f17df40c6ec15a07e01dc5ab575f0a1
Eliel Marques da Silva <elielmarques@protonmail.com>
2025-12-30 16:06:09 -0300

    30-12-2025

---
+++ aula 14/ambiente-2.js
@@ -0,0 +1,14 @@
+for (c=1;c<=1000;c++) {
+    console.log (`Parte ${c}.`)
+}
+
+
+
+
+/*var c = 1
+
+do {
+    console.log (c)
+    c++
+} while (c<=5)
+*/
---
+++ aula 14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1 
+do {
+    console.log(`c`)
+    c++
+} while (c<=10)
+*/
+
+
+
+for  (var c=1;c<=10;c++) {
+    console.log (c)
+}
