bc6c4c4d33229c9a0f05c4fc5ee41f19884de169
Eliel Marques da Silva <elielmarques@protonmail.com>
2026-01-23 01:33:14 -0300

    23-01-2026

---
+++ aula 16 - entenda os códigos/exercícios/Analisador de Números/cerebro.js
@@ -0,0 +1,32 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+
+//Análise de Dados
+
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+} 
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function Adicionar () {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok.')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
