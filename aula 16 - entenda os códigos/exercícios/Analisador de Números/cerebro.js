9655c06983fdd9b0c4898cdd72e3c6e2a0f9daf0
Eliel Marques da Silva <elielmarques@protonmail.com>
2026-01-25 01:58:59 -0300

    25-01-2025

--- aula 16 - entenda os códigos/exercícios/Analisador de Números/cerebro.js
+++ aula 16 - entenda os códigos/exercícios/Analisador de Números/cerebro.js
@@ -23,10 +23,49 @@ function inLista(n, l) {
 }
 
 
-function Adicionar () {
+function adicionar () {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('Tudo ok.')
+        //window.alert('Tudo ok.')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML= '' // zera a soma dos números quando um novo é adicionado
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+
+    //Apagar o texto depois de finalizado
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar () {
+        if (valores.length == 0) {
+            window.alert(`Adicione valores antes de finalizar!`)
+        } else {
+            let tot = valores.length //tot = números cadastrados
+
+            let maior = valores [0]
+            let menor = valores [0]
+            let soma = 0
+            let media = 0
+
+            for (let pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior)
+                    maior = valores[pos]
+                if (valores[pos] < menor)
+                    menor = valores[pos]
+            }
+            media = soma / tot
+            res.innerHTML = '' 
+            res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+            res.innerHTML += `<p>O maior número é ${maior}.</p>`
+            res.innerHTML += `<p>O menor número é ${menor}.</p>`
+            res.innerHTML += `<p>Somando todos, temos ${soma}</p>`
+            res.innerHTML += `<p>A média de todos os valores é ${media}</p>`
+            
+        }
 }
